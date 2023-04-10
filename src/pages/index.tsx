99ef5087630a248797a9a32d89f92dfeee2321e0
Mohamed Diop <yxngbizzy@gmail.com>
2023-04-10 13:11:42 -0700

    adding posts to tRPC Router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,10 +6,9 @@ import { api } from "@/utils/api";
 import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
   const user = useUser();
 
+  const { data } = api.posts.getAll.useQuery();
   return (
     <>
       <Head>
@@ -18,7 +17,18 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        {user.isSignedIn ? <SignOutButton /> : <SignInButton />}
+        {!user.isSignedIn ? (
+          <SignInButton />
+        ) : (
+          <>
+            <SignOutButton />
+            <div>
+              {data?.map((post) => (
+                <div key={post.id}>{post.content}</div>
+              ))}
+            </div>
+          </>
+        )}
       </main>
     </>
   );
