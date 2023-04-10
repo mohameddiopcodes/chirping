e9a81f2b7c8d27ee4d11440b7488b96fb5e2ca86
Mohamed Diop <yxngbizzy@gmail.com>
2023-04-10 15:29:49 -0700

    make post content bigger

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -45,7 +45,7 @@ const PostView = ({ data: { post, author } }: { data: PostWithUser }) => {
             post.createdAt
           ).fromNow()}`}</span>
         </div>
-        <span>{post.content}</span>
+        <span className="text-2xl">{post.content}</span>
       </div>
     </div>
   );
